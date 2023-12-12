62761c0b0b912a2eabd8b6a524fad30888b95633
Bahaa Ghali <81379753+bahaaghali000@users.noreply.github.com>
2023-12-12 17:38:27 +0200

    Update main.route.js

--- backend/routes/main.route.js
+++ backend/routes/main.route.js
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
       method: "POST",
       headers: {
         Authorization:
-          "Bearer " + "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",
+          "Bearer " + process.env.SECRET_KEY,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
